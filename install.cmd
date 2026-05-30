aa972faeac7a3031ecf8648dee5123b49fee5139
Lee0701 <hgl1002@naver.com>
2026-05-31 08:18:52 +0900

    Fix install script

--- install.cmd
+++ install.cmd
@@ -1,3 +1,9 @@
-copy /y "build\bin\Debug\libime-libhangul.dll" "C:\Program Files\libime-libhangul"
-regsvr32 "C:\Program Files\libime-libhangul\libime-libhangul.dll"
+set INSTALL_DIR=C:\Program Files\libime-libhangul
+mkdir "%INSTALL_DIR%"
+mkdir "%INSTALL_DIR%\hanja"
+copy /y "libhangul\data\hanja\hanja.txt" "%INSTALL_DIR%\hanja"
+copy /y "libhangul\data\hanja\freq-hanja.txt" "%INSTALL_DIR%\hanja"
+copy /y "libhangul\data\hanja\freq-hanjaeo.txt" "%INSTALL_DIR%\hanja"
+copy /y "build\bin\Debug\libime-libhangul.dll" "%INSTALL_DIR%"
+regsvr32 /s "%INSTALL_DIR%\libime-libhangul.dll"
 pause
--- uninstall.cmd
+++ uninstall.cmd
@@ -1,3 +1,5 @@
-regsvr32 /u "C:\Program Files\libime-libhangul\libime-libhangul.dll"
-del "C:\Program Files\libime-libhangul\libime-libhangul.dll"
+set INSTALL_DIR=C:\Program Files\libime-libhangul
+regsvr32 /u /s "%INSTALL_DIR%\libime-libhangul.dll"
+del "%INSTALL_DIR%\libime-libhangul.dll"
+rmdir /s /q "%INSTALL_DIR%"
 pause
